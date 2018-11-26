e9e9a2671e38cd9ec700f253ef4a803b892dedd5
hungphongbk <programmingd32@gmail.com>
2018-11-26 22:00:26 +0700

    fuck somethings

--- pm2.config.js
+++ pm2.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "web-panel",
-      script: "yarn",
+      script: "/usr/bin/yarn",
 
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
       args: "start",
