a673c29611c51642e8a6bc6afeec473a8c5fc2f1
hungphongbk <programmingd32@gmail.com>
2018-12-05 05:53:15 +0700

    SSL cert

--- server/jobs/SSLCert.js
+++ server/jobs/SSLCert.js
@@ -6,7 +6,7 @@ const generate = ({ uid, user, domain, webRoot }) => async (
 ) => {
   const homeDir = await _homeDir(user);
   let output = await _shellCommandAsync(
-    `whoami;${homeDir}/.acme.sh/acme.sh -v`,
+    `pwd`,
     {
       cwd: webRoot,
       uid,
